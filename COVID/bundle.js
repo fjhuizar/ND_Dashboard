5c0c1055a75559f69f8755062b38417dc3a4c0d4
Francisco J Huizar <fhuizar@nd.edu>
2020-08-16 17:07:51 -0400

    Update bundle.js

--- COVID/bundle.js
+++ COVID/bundle.js
@@ -26183,7 +26183,7 @@
 
     	$$invalidate('Time_to_death', Time_to_death     = 18.5); //https://www.drugs.com/medical-answers/covid-19-symptoms-progress-death-3536264/
     	$$invalidate('logN', logN              = Math.log(18521)); //Total population
-    	$$invalidate('I0', I0                = 44); //Initial infected
+    	$$invalidate('I0', I0                = 45); //Initial infected
     	$$invalidate('R0', R0                = 6.54); //Estimated R0 from: https://rt.live/us/IN
     	$$invalidate('D_incbation', D_incbation       = 5.4); //Estimated incubation time, usually between 5-6 days: https://www.who.int/docs/default-source/coronaviruse/situation-reports/20200402-sitrep-73-covid-19.pdf?sfvrsn=5ae25bc7_6#:~:text=The%20incubation%20period%20for%20COVID,persons%20can%20be%20contagious.
     	$$invalidate('D_infectious', D_infectious      = 5.0); //How long someone is infectious
@@ -26191,7 +26191,7 @@
     	$$invalidate('D_recovery_severe', D_recovery_severe = (22.0)); //https://www.drugs.com/medical-answers/covid-19-symptoms-progress-death-3536264/
     	$$invalidate('D_hospital_lag', D_hospital_lag    = 12);//https://www.drugs.com/medical-answers/covid-19-symptoms-progress-death-3536264/
     	$$invalidate('CFR', CFR               = 0.023); // Fatlity Rate
-    	$$invalidate('InterventionTime', InterventionTime  = 14);
+    	$$invalidate('InterventionTime', InterventionTime  = 10);
     	$$invalidate('OMInterventionAmt', OMInterventionAmt = 1/3);
     	Time              = 100;
     	$$invalidate('Xmax', Xmax              = 110000);
