05601756586972bdc976c1bef38b846f013a2012
Francisco J Huizar <fhuizar@nd.edu>
2020-08-18 18:00:22 -0400

    Updated CFR

--- COVID/bundle.js
+++ COVID/bundle.js
@@ -22766,7 +22766,7 @@
     			link = element("link");
     			t0 = space();
     			h2 = element("h2");
-    			h2.textContent = "Notre Dame COVID Predictions Dashboard (Last Updated 8/18/2020 9:30AM EST)";
+    			h2.textContent = "Notre Dame COVID Predictions Dashboard (Last Updated 8/18/2020 6:00PM EST)";
     			t2 = space();
     			div75 = element("div");
     			div50 = element("div");
@@ -26200,7 +26200,7 @@
     	$$invalidate('D_recovery_mild', D_recovery_mild   = (11)); // https://www.drugs.com/medical-answers/covid-19-symptoms-progress-death-3536264/
     	$$invalidate('D_recovery_severe', D_recovery_severe = (22.0)); //https://www.drugs.com/medical-answers/covid-19-symptoms-progress-death-3536264/
     	$$invalidate('D_hospital_lag', D_hospital_lag    = 12);//https://www.drugs.com/medical-answers/covid-19-symptoms-progress-death-3536264/
-    	$$invalidate('CFR', CFR               = (0.5*0.023+0.013488*0.5)*(1-0.72)+0.000042*0.72); // Fatlity Rate
+    	$$invalidate('CFR', CFR               = 0.003*(1-0.72)+0.000042*0.72); // Fatlity Rate
     	$$invalidate('InterventionTime', InterventionTime  = 3);
     	$$invalidate('OMInterventionAmt', OMInterventionAmt = 9/10);
     	Time              = 98;
