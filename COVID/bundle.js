8129e7f728be6899baeb2fa7d1967ff1b83c97f3
Francisco J Huizar <fhuizar@nd.edu>
2020-08-24 22:31:10 -0400

    Another intervention day change.

--- COVID/bundle.js
+++ COVID/bundle.js
@@ -22767,7 +22767,7 @@
     			link = element("link");
     			t0 = space();
     			h2 = element("h2");
-    			h2.textContent = "Notre Dame COVID Predictions Dashboard (Last Updated 8/24/2020 10:15PM EST)";
+    			h2.textContent = "Notre Dame COVID Predictions Dashboard (Last Updated 8/24/2020 10:30PM EST)";
     			t2 = space();
     			div75 = element("div");
     			div50 = element("div");
@@ -26202,7 +26202,7 @@
     	$$invalidate('D_recovery_severe', D_recovery_severe = (22.0)); //https://www.drugs.com/medical-answers/covid-19-symptoms-progress-death-3536264/
     	$$invalidate('D_hospital_lag', D_hospital_lag    = 12);//https://www.drugs.com/medical-answers/covid-19-symptoms-progress-death-3536264/
     	$$invalidate('CFR', CFR               = 0.003*(1-0.72)+0.000042*0.72); // Fatlity Rate
-    	$$invalidate('InterventionTime', InterventionTime  = 3);
+    	$$invalidate('InterventionTime', InterventionTime  = 1);
     	$$invalidate('OMInterventionAmt', OMInterventionAmt = 9.4/10);
     	Time              = 94;
     	$$invalidate('Xmax', Xmax              = 110000);
